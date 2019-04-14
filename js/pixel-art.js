bd3724714cc089d557dba6327bc10e6596bae8c6
unknown <juan23davila@gmail.com>
2019-04-14 15:42:12 -0500

    Se completa guía número 2

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -23,23 +23,45 @@ var nombreColores = ['White', 'LightYellow',
 // Variable para guardar el elemento 'color-personalizado'
 // Es decir, el que se elige con la rueda de color.
 var colorPersonalizado = document.getElementById('color-personalizado');
+let colorIndicador     = document.getElementById('indicador-de-color');
 let paleta             = document.getElementById('paleta');
 let grillaPixs         = document.getElementById('grilla-pixeles');
+let clickStatus        = false;
 
 $(document).ready(function(){
   createColorPalette();
   createPixelGrid();
 });
 
+/**
+ * Se modifica color personalizado
+ */
 colorPersonalizado.addEventListener('change', 
   (function() {
     // Se guarda el color de la rueda en colorActual
     colorActual = colorPersonalizado.value;
     // Completar para que cambie el indicador-de-color al colorActual
-
+    colorIndicador.style.backgroundColor = colorActual;
 
   })
 );
+
+/**
+ * Modifica un indicador de color cuando un color es seleccionado de la paleta de colores
+ */
+function changeIndicadorColor(e){
+  let colorClicked = e.target.style.backgroundColor;
+  colorIndicador.style.backgroundColor = colorClicked;
+}
+
+/**
+ * Modifica color del pixel seleccionado al que se encuentra en el indicador cuando el usuario hace clic.
+ */
+function changeGridPXColor(e){
+  if(clickStatus){
+    e.target.style.backgroundColor = colorIndicador.style.backgroundColor;
+  }
+}
 
 /**
  * Crea la paleta de colores
@@ -51,7 +73,19 @@ function createColorPalette(){
     newDiv = document.createElement("div");
     newDiv.className = "color-paleta";
     newDiv.style.backgroundColor = nombreColores[i];
+    newDiv.addEventListener("click", changeIndicadorColor);
     paleta.appendChild(newDiv);
+  }
+}
+
+function changeClickStatus(e){
+  if(e.type === "mousedown"){
+    clickStatus = true;
+    e.target.style.backgroundColor = colorIndicador.style.backgroundColor;
+  }
+  
+  if(e.type === "mouseup"){
+    clickStatus = false;
   }
 }
 
@@ -65,4 +99,7 @@ function createPixelGrid(){
     newDiv = document.createElement("div");
     grillaPixs.appendChild(newDiv);
   }
+  grillaPixs.addEventListener("mouseover", changeGridPXColor);
+  grillaPixs.addEventListener("mousedown", changeClickStatus);
+  grillaPixs.addEventListener("mouseup", changeClickStatus);
 }
